d8dc771e897cc9c19b22c06023f68382fb1868d0
Garth Humphreys <garthhumphreys@gmail.com>
2020-06-21 00:49:13 -0500

    imported from websnack private repo

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer mt-auto py-3">
+    <div class="container">
+      <span>
+        <div class="font-weight-bold">AS AT: 4 MAY 2020, 3PM</div>
+        <small>*A person who initially tested positive and who has subsequently tested negative for the COVID-19 virus in two tests done at least 24 hours apart 14 daysafter their positive test, or after their symptoms have gone away, whichever is longer.</small>
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,99 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <div v-for="report in reports" :key="report.id">
+        {{ report.id }}
+        {{ report.todaysConfirmed }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo,
-  },
-}
+  export default {
+    components: {
+    },
+    data() {
+      return {
+        reports: [
+          {
+            id: 1,
+            todaysConfirmed: 1,
+            recovered: 55,
+            active: {
+              count: 38,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            totalConfirmed: 94,
+            tested: 5900,
+            deaths: 1,
+            worldwide: {
+              count: 4508435,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            gender: {
+              male: 1,
+              female: 1,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            age: {
+              start: 14,
+              end: 85,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            severity: {
+              asymptomatic: 29,
+              symptomatic: 9,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            hospital: {
+              hospitalized: 0,
+              nonHospitalized: 38,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            isolation: {
+              count: 163,
+              created: "2020-03-03T18:25:43.511Z"
+            },
+            created: "2020-03-03T18:25:43.511Z",
+            author: 1
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding-top: 15px;
+  }
 </style>
